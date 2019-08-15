a7870b57a95dd65d382da7b83f8940ffb3648697
Jennifer <jenalexsarah@gmail.com>
2019-08-15 14:23:23 -0400

    DOM obj identifying completed practice

---
+++ script.js
@@ -0,0 +1,140 @@
+// Create a new repo with an index.html file and a JavaScript file. Link your JavaScript file to your HTML document. You can use your basicProj bash function for this.
+// Add an empty h1 element to your HTML document. Give it an id of "main-heading"
+// Use document.querySelector to get a reference to your h1 element
+// Use .innerHTML to give your h1 element the text "My Awesome Website"
+
+// mainHeading = document.querySelector("#main-heading")
+// mainHeading.innerHTML ="My Awesome Website"
+
+// // ------------- Lightning Exercise 2 ------------------------------//
+// // Refactor the following functions as fat arrow functions:
+
+// const checkIfEven= (number) =>{
+//     if(number%2===0){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// const equalSame = checkIfEven(4)
+// console.log(equalSame)
+
+// const addTwoNumbers= (num1, num2) => num1 + num2
+// const sumTwoNumbers = addTwoNumbers(2,2)
+// console.log(sumTwoNumbers)
+
+// const makeASandwich= (breadType, toppings) => 
+//  `Here's your ${toppings} sandwich on ${breadType}`
+
+// const mySandwich = makeASandwich("wheat", "tomatoes")
+// console.log(mySandwich)
+
+// practice excercises
+
+// paste student array
+const students = [
+    {
+        name: "Chris Miller",
+        subject: "History",
+        info: "Failed last exam",
+        score: 59
+    },
+    {
+        name: "Courtney Seward",
+        subject: "History",
+        info: "Has completed all homework",
+        score: 91
+    },
+    {
+        name: "Garrett Ward",
+        subject: "History",
+        info: "Wonderful at helping other students",
+        score: 88
+    },
+    {
+        name: "John Dulaney",
+        subject: "History",
+        info: "Has never missed a class or exam",
+        score: 92
+    },
+    {
+        name: "Greg Lawrence",
+        subject: "History",
+        info: "Sub-par performance all around",
+        score: 64
+    },
+    {
+        name: "Leah Duvic",
+        subject: "History",
+        info: "Wonderful student",
+        score: 97
+    },
+    {
+        name: "Jesse Page",
+        subject: "History",
+        info: "Smokes too much. Distracting.",
+        score: 76
+    },
+    {
+        name: "Kevin Haggerty",
+        subject: "History",
+        info: "Falls asleep in class",
+        score: 79
+    },
+    {
+        name: "Max Wolf",
+        subject: "History",
+        info: "Talks too much",
+        score: 83
+    },
+    {
+        name: "Lissa Goforth",
+        subject: "History",
+        info: "Asks pointless, unrelated questions",
+        score: 78
+    },
+    {
+        name: "Tyler Bowman",
+        subject: "History",
+        info: "When was the last time he attended class?",
+        score: 48
+    },
+    {
+        name: "Ray Medrano",
+        subject: "History",
+        info: "Needs to contribute to in-class discussions",
+        score: 95
+    }
+]
+
+// copy createSTudentComponent function
+
+const createStudentComponent = (students, className) => {
+    return `
+        <div class="student">
+            <h1 class= ${className}>${students.name}</h1>
+            <section>${students.subject}</section>
+            <aside>${students.info}</aside>
+        </div>
+    `
+}
+
+// Iterate the array of students, and apply the correct style to the h1 depending on the score of the student being below 60, or above it.
+
+for (let i = 0; i < students.length; i++) {
+     let studentComponent = ""
+    if (students[i].score >= 60) {
+        studentComponent = createStudentComponent(students[i],"passing")
+        
+    } else {
+        studentComponent = createStudentComponent(students[i],"failing")
+    }
+
+// send to DOM
+const container = document.querySelector("#container")
+    container.innerHTML += studentComponent
+
+}
+
+
